e5e6589d31652ce878b540622317342c5737a6bc
Jeliel <jeliel.augusto@icomp.ufam.edu.br>
2018-07-19 17:23:57 -0400

    Lógica e script para cadastro e login prontos, falta o webservice

--- build/js/session.js
+++ build/js/session.js
@@ -1,23 +1,79 @@
+var sessao;
 $(document).ready(function () {
-    //
-    // Define your database
-    //
     var db = new Dexie("dbusuario");
+    var userSession = {};
     db.version(1).stores({
         user: '&id,cpf,email'
     });
-    db.open().catch (function (err) {
+    db.open().catch(function (err) {
         console.error('Failed to open db: ' + (err.stack || err));
     });
-    db.user.get('1').then((res)=>{
-        if(res === undefined){
-            db.user.put({id:'1',isLogado:'false',nome:'',cpf:'',email:''}).then(()=>{
+    db.user.get('1').then((res) => {
+        if (res === undefined) {
+            db.user.put({ id: '1', isLogado: false, nome: '', cpf: '', email: '' }).then(() => {
                 console.log('Criado');
+                userSession = { id: '1', isLogado: false, nome: '', cpf: '', email: '' };
             });
-        }else{
-            console.log('Existe');
+        } else {
+            userSession = res;
         }
-    }).catch((err)=>{
+    }).catch((err) => {
         console.error('Failed to open db: ' + (err.stack || err));
     });
+    function logar() {
+        
+    }
+    function deslogar() {
+
+    }
+    function inscrever() {
+
+    }
+    function isLogado() {
+        return userSession.isLogado;
+    }
+    sessao = (function () {
+        return {
+            logar: logar,
+            isLogado: isLogado,
+            deslogar: deslogar,
+            inscrever: inscrever,
+        }
+    })();
 });
+
+function areaRestrita() {
+    if (!sessao.isLogado()) {
+        var location =  'file:///home/jeliel/Documentos/Trabalho/site-encontro-aquicultura/aquicultura/build';
+        window.location.href = location + '/login.html';
+    }else{
+
+    }
+}
+function showCadastro(){
+    var esconder = document.getElementById('login-form');
+    esconder.classList.add('out');
+    var mostrar = document.getElementById('cadastro-form');
+    mostrar.classList.add('in');
+    mostrar.style.display = 'block';
+}
+function cadastrar(event){
+    event.preventDefault();
+    var emailElement,senhaElement,confElement,cpfElement;
+    var email,senha,confsenha,cpf;
+    emailElement = document.getElementById('email');
+    senhaElement = document.getElementById('senha');
+    confElement = document.getElementById('confsenha');
+    cpfElement = document.getElementById('cpf');
+    email = emailElement.value;
+    senha = senhaElement.value;
+    confsenha = confElement.value;
+    cpf = cpfElement.value;
+    if(confsenha !== senha){
+        confElement.style.borderColor = 'yellow';
+        senhaElement.style.borderColor = 'yellow';
+        var error = document.getElementById('erro_form');
+        error.style.display = 'block';
+        error.innerText = 'O campo confirmar senha está diferente do campo senha';
+    }
+}
