061bc6ee79da5d6a4872e6852d96c072b85b412f
Jeliel <jeliel.augusto@icomp.ufam.edu.br>
2018-07-20 19:35:14 -0400

    Área restrita, login, cadastro e orientação para submissão

--- build/js/session.js
+++ build/js/session.js
@@ -2,13 +2,29 @@ var sessao;
 $(document).ready(function () {
     var db = new Dexie("dbusuario");
     var userSession = {};
+    var userRedirect = {};
     db.version(1).stores({
-        user: '&id,cpf,email'
+        user: '&id,cpf,email',
+        redirect: '&id'
     });
-    db.open().catch(function (err) {
-        console.error('Failed to open db: ' + (err.stack || err));
+    db.open().catch(function (e) {
+        console.error("Open failed: " + e);
     });
-    db.user.get('1').then((res) => {
+    /*---Get para objeto de redirecionar---*/
+    db.redirect.get('1')
+    .then((res)=>{
+        if(res === undefined){
+            db.redirect.put(objetoRedirect()).then(()=>{
+                console.log('Criado redirect');
+                userRedirect = objetoRedirect();
+            });
+        }else{
+            userRedirect = res;
+        }
+    });
+    /*---Get para objeto de sessão---*/
+    db.user.get('1')
+    .then((res) => {
         if (res === undefined) {
             db.user.put({ id: '1', isLogado: false, nome: '', cpf: '', email: '' }).then(() => {
                 console.log('Criado');
@@ -20,14 +36,32 @@ $(document).ready(function () {
     }).catch((err) => {
         console.error('Failed to open db: ' + (err.stack || err));
     });
-    function logar() {
-        
+    function logar(dados,callback) {
+        db.user.put({ id: '1', isLogado: true, nome: dados.nome, cpf: dados.cpf, email: dados.email }).then(() => {
+            userSession = { id: '1', isLogado: true, nome: dados.nome, cpf: dados.cpf, email: dados.email};
+        })
+        .then(()=>{
+            if(callback){
+                callback();
+            }
+        })
     }
-    function deslogar() {
-
+    function deslogar(callback) {
+        db.user.put({id: '1',isLogado: false,nome:'',cpf:'',email: ''})
+        .then(()=>{
+            if(callback){
+                callback();
+            }
+        });
     }
-    function inscrever() {
-
+    function getDados() {
+        return userSession;
+    }
+    function setRed(red){
+        db.redirect.put({id:'1',redirect:red}).then(()=>{});
+    }
+    function getRed(){
+        return userRedirect;
     }
     function isLogado() {
         return userSession.isLogado;
@@ -37,19 +71,31 @@ $(document).ready(function () {
             logar: logar,
             isLogado: isLogado,
             deslogar: deslogar,
-            inscrever: inscrever,
+            getDados: getDados,
+            setRed: setRed,
+            getRed: getRed
         }
     })();
 });
-
+function objetoRedirect(){
+    return {id:'1',redirect:''}
+}
 function areaRestrita(redirecionar) {
-    if (!sessao.isLogado()) {
+    if (sessao.isLogado()) {
+        if(redirecionar === "inscricao"){
+        
+        }
+        else if(redirecionar === "trabalhos"){
+            var location =  'file:///home/jeliel/Documentos/Trabalho/site-encontro-aquicultura/aquicultura/build';
+            window.location.href = location + '/arearestrita.html';    
+        }else{
+            var location =  'file:///home/jeliel/Documentos/Trabalho/site-encontro-aquicultura/aquicultura/build';
+            window.location.href = location + '/arearestrita.html';    
+        }
+    }else{
+        sessao.setRed(redirecionar); //Guardar para onde o usuário queria ir
         var location =  'file:///home/jeliel/Documentos/Trabalho/site-encontro-aquicultura/aquicultura/build';
         window.location.href = location + '/login.html';
-    }else if(redirecionar === "login"){
-        //Ir para área restrita
-    }else if(redirecionar === "inscricao"){
-        //Redirecionar para as inscrições
     }
 }
 function showCadastro(){
@@ -59,7 +105,31 @@ function showCadastro(){
     mostrar.classList.add('in');
     mostrar.style.display = 'block';
 }
-function cadastrar(event){
+function logout(){
+    sessao.deslogar(()=>{
+        var location =  'file:///home/jeliel/Documentos/Trabalho/site-encontro-aquicultura/aquicultura/build';
+        window.location.href = location + '/index.html';
+    });
+}
+function login(event){
+    event.preventDefault();
+    var cpf, senha;
+    senha = document.getElementById('senha').value;
+    cpf = document.getElementById('cpf').value;
+    //Chamar webservice com dados
+    dados = {
+        nome: 'Jeliel',
+        cpf: '004.238.642-03',
+        email: 'jeliel.augusto10@gmail.com'
+    };
+    sessao.logar(dados,()=>{ //callback para quando o login foi feito
+        //Redirecionar para a página que o usuário queria acessar
+        // sessao.getRed().then((res)=>{
+        //     areaRestrita(res);
+        // })
+    });
+}
+function cadastrar(event,redirecionar){
     event.preventDefault();
     var emailElement,senhaElement,confElement,cpfElement;
     var email,senha,confsenha,cpf;
@@ -78,5 +148,11 @@ function cadastrar(event){
         error.style.display = 'block';
         error.innerText = 'O campo confirmar senha está diferente do campo senha';
     }
-    //Se tiver sucesso, redirecionar para área restrita.
+    //Se tiver sucesso, redirecionar.
+    /*
+    if(ja_tem_login || session.isLogado()){
+        //redirecionar 
+    }
+    */
+    areaRestrita(redirecionar);
 }
