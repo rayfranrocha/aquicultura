ac5dc16d27e2abbceb5f3f31ed2ece63b2b8434b
jeliel <jeliel.augusto10@gmail.com>
2018-07-25 17:25:40 -0400

    Responsividade (50%)

--- build/js/default.js
+++ build/js/default.js
@@ -1,20 +1,38 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    autoplay:true,
-    autoplayTimeout:1000,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    if (width <= 550) {
+        /* -----TROCA POSIÇÃO BOTÕES----- */
+        var botoesDiv = document.getElementById('botoesDiv');
+        var btAnterior = document.getElementById('anterior');
+        var btProximo = document.getElementById('proximo');
+        if (btAnterior && btProximo && botoesDiv) {
+            botoesDiv.removeChild(btAnterior);
+            botoesDiv.appendChild(btAnterior);
+        }
+        /* ---- TABELA ----------------- */
+        var tabela = document.getElementById('tabela-minicursos');
+        var mostrarModalMinicursos = document.getElementById('mostrarModalMinicursos');
+        if(tabela){
+            tabela.style.display = 'none';
+            mostrarModalMinicursos.style.display = 'block'; 
         }
     }
-  });
 }); 
