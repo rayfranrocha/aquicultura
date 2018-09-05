17a69d030c8a765bb73cc2c93f064dd8e81943cc
silasjr <jose.silasjr.bb@gmail.com>
2018-09-04 22:14:12 -0400

    implementado nova visao de produtos para o cliente e funcionalidade de reimprimir boleto e excluir inscricao.

--- build/js/components/inscricao/produtos.vue
+++ build/js/components/inscricao/produtos.vue
@@ -6,47 +6,66 @@
             <div class="card-body text-center">
                 <p v-if="listInscricao.length === 0">Ainda não há itens comprados</p>
 
-                <div class="container">
+                <div v-else class="container">
                     <div class="row">
-                        <div class="col" v-if="inscricaoEvento">
+                        <div class="col-sm-12"
+                            v-for="inscricaoEvento in listInscricao"
+                            :key="inscricaoEvento._id">
                             <div class="card border-info">
-                                <h6 class="card-title u-margin-top-small">Inscrição no Evento</h6>
+                                <h6 class="card-title u-margin-top-small">Produtos Adiquiridos nessa inscrição</h6>
                                 <hr class="small">
                                 <div class="card-body">
                                     <div class="container">
-                                        <ul class="lista-dados">
-                                            <li class="dado">Tipo de Inscrição: {{inscricaoEvento.tipoInscricao || inscricaoEvento.tipoInscricaoObj.nome}}</li>
-                                            <li class="dado">Total Pago: {{formataMoney(inscricaoEvento.valorInscricao)}}</li>
-                                        </ul>
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered table-sm">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Descriçao</th>
+                                                        <th>Valor (R$)</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr v-if="(inscricaoEvento.tipoInscricao && inscricaoEvento.tipoInscricao !== 'MINI_CURSO') || inscricaoEvento.tipoInscricaoObj">
+                                                        <td class="text-left">Inscrição no evento como: {{inscricaoEvento.tipoInscricao || inscricaoEvento.tipoInscricaoObj.nome}}</td>
+                                                        <td>{{getPrecoInscricao(inscricaoEvento)}}</td>
+                                                    </tr>
+                                                    <tr v-if="inscricaoEvento.minicurso">
+                                                        <td class="text-left">Inscrição no minicurso 1: {{inscricaoEvento.minicurso.nome}}</td>
+                                                        <td>{{formataMoney(inscricaoEvento.minicurso.preco)}}</td>
+                                                    </tr>
+                                                    <tr v-if="inscricaoEvento.minicurso2">
+                                                        <td class="text-left">Inscrição no minicurso 2: {{inscricaoEvento.minicurso2.nome}}</td>
+                                                        <td>{{formataMoney(inscricaoEvento.minicurso2.preco)}}</td>
+                                                    </tr>
+                                                </tbody>
+                                                <tfoot>
+                                                    <tr>
+                                                        <td class="text-right font-weight-bold">Total (R$):</td>
+                                                        <td class="font-weight-bold">{{formataMoney(inscricaoEvento.totalAPagar)}}</td>
+                                                    </tr>
+                                                </tfoot>
+                                            </table>
+                                        </div>
+                                        <small v-if="podeGerarBoleto(inscricaoEvento)">
+                                            <b>Atenção:</b> Se você ainda não realizou o pagamento dessa inscrição e o boleto estiver vencido, exclua-a e faça uma nova.
+                                        </small>
                                     </div>
                                 </div>
                                 <div class="card-footer text-left">
-                                    <label for="pagamentoEvento">Pagamento: <b>{{getStatusPagamento(inscricaoEvento)}}</b></label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="w-100"></div>
-                        <div class="col-md-6 col-xs-12" v-if="inscricaoMinicurso">
-                            <div class="card border-info">
-                                <h6 class="card-title">Inscrição no Minicurso 1</h6>
-                                <div class="card-body">
-                                    <p class="card-text">{{inscricaoMinicurso.minicurso.nome}}</p>
-                                    <p class="card-text">Total Pago: {{formataMoney(inscricaoMinicurso.minicurso.preco)}}</p>
-                                </div>
-                                <div class="card-footer text-left">
-                                    <label for="pagamentoEvento">Pagamento: <b>{{getStatusPagamento(inscricaoMinicurso)}}</b></label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-xs-12" v-if="inscricaoMinicurso2">
-                            <div class="card border-info">
-                                <h6 class="card-title">Inscrição no Minicurso 2</h6>
-                                <div class="card-body">
-                                    <p class="card-text">{{inscricaoMinicurso2.minicurso2.nome}}</p>
-                                    <p class="card-text">Total Pago: {{formataMoney(inscricaoMinicurso2.minicurso2.preco)}}</p>
-                                </div>
-                                <div class="card-footer text-left">
-                                    <label for="pagamentoEvento">Pagamento: <b>{{getStatusPagamento(inscricaoMinicurso2)}}</b></label>
+                                    <div class="float-sm-left">
+                                        <label for="pagamentoEvento">Status: <b>{{getStatusPagamento(inscricaoEvento)}}</b></label>
+                                    </div>
+                                    <div class="float-sm-right">
+                                        <button v-if="podeExcluir(inscricaoEvento)" @click="remover(inscricaoEvento._id)" class="btn btn-danger">
+                                            <i class="fa fa-trash"></i>
+                                            Excluir inscrição
+                                        </button>
+                                        <a v-if="podeGerarBoleto(inscricaoEvento)" :href="linkBoleto(inscricaoEvento)" class="btn btn-primary"
+                                            target="_blank">
+                                            <i class="fa fa-file-pdf"></i>
+                                            Imprimir Boleto
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -102,7 +121,7 @@
                 return window.localStorage.getItem('user');
             },
             isMinicursoDisponivel () {
-                return !this.inscricaoMinicurso || !this.inscricaoMinicurso2;
+                return !this.inscricaoMinicurso || !this.inscricaoMinicurso2
             }
         },
         data () {
@@ -114,40 +133,90 @@
             }
         },
         created() {
-            axios.get(`/inscricao?user=${this.idUser}`)
-                .then(response => {
-                    this.listInscricao = response.data;
-                    this.inscricaoEvento = this.listInscricao.find(inscricao => inscricao.tipoInscricao || inscricao.tipoInscricaoObj);
-                    this.inscricaoMinicurso = this.listInscricao.find(inscricao => inscricao.minicurso);
-                    this.inscricaoMinicurso2 = this.listInscricao.find(inscricao => inscricao.minicurso2);
-                });
+            this.loadInscricao();
         },
         methods: {
+            loadInscricao() {
+                axios.get(`/inscricao?user=${this.idUser}`)
+                    .then(response => {
+                        this.listInscricao = response.data;
+                        this.inscricaoEvento = this.listInscricao.find(inscricao => inscricao.tipoInscricao || inscricao.tipoInscricaoObj);
+                        this.inscricaoMinicurso = this.listInscricao.find(inscricao => inscricao.minicurso);
+                        this.inscricaoMinicurso2 = this.listInscricao.find(inscricao => inscricao.minicurso2);
+
+                        if (this.inscricaoEvento) {
+                            if (this.inscricaoEvento.tipoInscricao === 'MINI_CURSO') {
+                                this.inscricaoEvento = null;
+                            }
+                        }
+                    });
+            },
             formataMoney (valor) {
                 if (!valor) return '';
                 return `R$ ${valor.toLocaleString('pt-BR',{minimumFractionDigits: 2, maximumFractionDigits: 2})}`
             },
             getStatusPagamento(inscricao) {
-            if (!inscricao) {
-                return ''
-            }
+                if (!inscricao) {
+                    return ''
+                }
 
-            if (inscricao.statusPagseguro && inscricao.statusPagseguro.transaction.status[0] === '3') {
-                return 'PAGAMENTO CONFIRMADO'
-            }
+                if (inscricao.statusPagseguro && inscricao.statusPagseguro.transaction.status[0] === '3') {
+                    return 'PAGAMENTO CONFIRMADO'
+                }
 
-            if (inscricao.statusPagseguro && inscricao.statusPagseguro.transaction.status[0] === '1') {
-                return 'AGUARDANDO PAGAMENTO'
-            }
+                if (inscricao.statusPagseguro && inscricao.statusPagseguro.transaction.status[0] === '1') {
+                    return 'AGUARDANDO PAGAMENTO'
+                }
 
-            if (inscricao.statusPagseguro && inscricao.statusPagseguro.transaction.status[0] === '2') {
-                return 'EM ANÁLISE PELO PAGSEGURO'
-            }
+                if (inscricao.statusPagseguro && inscricao.statusPagseguro.transaction.status[0] === '2') {
+                    return 'EM ANÁLISE PELO PAGSEGURO'
+                }
 
-            if (inscricao.pagseguro) {
-                return 'PROCESSANDO PAGAMENTO'
+                if (inscricao.pagseguro) {
+                    return 'FORMA DE PAGAMENTO NÃO DETECTADA (exclua essa inscrição e faça novamente)'
+                }
+            },
+
+            getPrecoInscricao (inscricao) {
+                if (inscricao.tipoInscricaoObj) {
+                    var dataBase = new Date();
+                    dataBase.setDate(21);
+                    dataBase.setMonth(8);
+
+                    var dataAtual = new Date();
+                    if (dataAtual < dataBase) {
+                        return this.formataMoney(inscricao.tipoInscricaoObj.precoAte21);
+                    } else {
+                        return this.formataMoney(inscricao.tipoInscricaoObj.precoApos21);
+                    }
+                    
+                } else {
+                    return this.formataMoney(inscricao.valorInscricao);
+                }
+            },
+
+            remover(idInscricao) {
+                if (confirm('Deseja relmente Excluir essa Inscrição?')) {
+                    axios.delete(`/inscricao/${idInscricao}`)
+                        .then(() => this.loadInscricao());
+                }
+            },
+
+            podeExcluir (inscricao) {
+                return !inscricao.pagseguro || !inscricao.statusPagseguro 
+                        || inscricao.statusPagseguro.transaction.status[0] === '1';
+            },
+
+            podeGerarBoleto (inscricao) {
+                return (inscricao.statusPagseguro && inscricao.statusPagseguro.transaction
+                    && inscricao.statusPagseguro.transaction.paymentMethod[0].code[0] === '202'
+                    && inscricao.statusPagseguro.transaction.status[0] === '1');
+            },
+
+            linkBoleto (inscricao) {
+                return this.podeGerarBoleto(inscricao) ?
+                    inscricao.statusPagseguro.transaction.paymentLink[0]: '#'
             }
-        }
         }
     }
 </script>
